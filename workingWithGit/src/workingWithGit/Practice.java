f56b675890ee4d3e3dde5c03bc62df70e25d8e84
liuda1 <liudashyshko@gmail.com>
2017-01-30 18:57:32 -0500

    reverse string

---
+++ workingWithGit/src/workingWithGit/Practice.java
@@ -0,0 +1,13 @@
+package workingWithGit;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		String str="Manipulation";
+		for (int i = str.length()-1; i >= 0; i--) {
+			System.out.print(str.charAt(i)+ " ");
+		}
+
+	}
+
+}
