f745ff0e9c5e7c105cc3696435499b67217fe4fb
liuda1 <liudashyshko@gmail.com>
2017-01-30 19:04:12 -0500

    printing elements in array

--- workingWithGit/src/workingWithGit/Practice.java
+++ workingWithGit/src/workingWithGit/Practice.java
@@ -7,7 +7,12 @@ public class Practice {
 		for (int i = str.length()-1; i >= 0; i--) {
 			System.out.print(str.charAt(i)+ " ");
 		}
-
+		Object[] x = new Object[]{1,2,3,"srk"};
+		for(Object o: x){
+			   System.out.println(o);
+			}	
 	}
 //edding line from gitHub
+	
+	
 }
